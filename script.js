c7c5acbee0f6d5c7772e81c2a127a96402c0cc07
Alonesa <alenakrasnov@mail.ru>
2020-09-09 22:13:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+var elements = document.querySelectorAll("li");
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", function(e) {
+	e.target.classList.toggle("done");
+});
+
+function addButtons() {
+	for(var i = 0; i < elements.length; i++){
+		var buttonDelete = document.createElement('button');
+		buttonDelete.textContent = 'delete';
+		elements[i].appendChild(buttonDelete);
+	}
+};
+
+addButtons()
+
+
